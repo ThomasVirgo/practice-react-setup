eb8901d0f08d57ef1f704b7d14ce5aef2cb000b1
Tom Virgo <tomcvirgo@gmail.com>
2021-08-06 16:42:20 +0100

    style nav bar

--- src/layout/Nav/index.js
+++ src/layout/Nav/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './style.css'
 
 const Nav = () => {
     return (
-        <>
-        <NavLink exact to='/activity' activeClassName='active'/>
-        <NavLink exact  to='/universities' activeClassName='active' />
-        <NavLink exact  to='/' activeClassName='active'/>
-        </>
+        <div className='nav-container'>
+        <NavLink className = 'nav-link' exact to='/activity' activeClassName='active'>Activity</NavLink>
+        <NavLink className = 'nav-link' exact  to='/universities' activeClassName='active'>Universities</NavLink>
+        <NavLink className = 'nav-link' exact  to='/' activeClassName='active'>Home</NavLink>
+        </div>
     )
 }
 
